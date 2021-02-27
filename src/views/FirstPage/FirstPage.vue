aea9455b3fde727f5e5d70604b00be3b336dd19f
Ilya Baskakov <baskakov.11111@mail.ru>
2021-02-27 19:38:23 +0300

    fst

---
+++ src/views/FirstPage/FirstPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <!-- <div class="main__form-wrap">
+      <form class="main__form">
+        <input type="text" placeholder="Как зовут ребенка?" />
+        <input type="text" placeholder="Возраст?" />
+        <input type="text" placeholder="За что похвалить?" />
+        <input type="text" placeholder="Email?" />
+        <input type="text" placeholder="Ваш текст" />
+      </form>
+    </div>
+    <div class="main__ellipse">
+      <div class="main__ellipse-text"></div>
+    </div>
+    <div class="main__tree"></div> -->
+    <div class="main__header"></div>
+    <div class="main__body">
+      <div class="body__row1">
+        <div class="body__yandex"><h1>Yandex</h1></div>
+      </div>
+      <div class="body__row2">
+        <div class="body__ellipse"><h1>Ellipse</h1></div>
+      </div>
+      <div class="body__row3">
+        <div class="body__form"><h1>Form</h1></div>
+        <div class="body__tree"><h1>Tree</h1></div>
+        <div class="body__pidor"><h1>Pidor</h1></div>
+      </div>
+    </div>
+    <div class="main__footer"></div>
+  </div>
+</template>
